048289fe1c79a54fd57ac9a8c3048e5fc8d4bea6
Antonio Sanjuan <tonysanj13@gmail.com>
2023-05-17 21:17:08 +0200

    fix: configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { CoreModule } from './modules/core/core.module';
 import { SharedModule } from './modules/shared/shared.module';
 import { APP_ROUTES } from './modules/routing/routing.module';
 import { MainLayoutModule } from './layouts/main-layout/main-layout.module';
-import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
