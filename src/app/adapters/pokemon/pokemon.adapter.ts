25ed1c3b3d19d2d4db888f2ea1405bb859a7c6c6
Tony Sanjuan <tonysanj13@gmail.com>
2023-05-24 16:02:52 +0200

    second version: usage of subscribers with dummy adapter

--- src/app/adapters/pokemon/pokemon.adapter.ts
+++ src/app/adapters/pokemon/pokemon.adapter.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { PokemonsDto } from 'src/app/models/dtos/pokemonsDto.model';
 import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Observable, forkJoin } from 'rxjs';
 import { PokemonDto } from 'src/app/models/dtos/pokemonDto.model';
 import { MinifiedPokemonDto } from 'src/app/models/dtos/pokemonMinified.model';
+import { Pokemon, Pokemons } from 'src/app/models/internals/pokemons.model';
 
 @Injectable()
 export class PokemonAdapter {
@@ -13,17 +14,42 @@ export class PokemonAdapter {
 
   constructor(private pokemonService: PokemonService) {}
 
-  public getPokemons(page: number): Observable<PokemonDto[]> {
+  public getPokemons(page: number): Observable<Pokemons> {
     this.isLoading = true;
     this.isError = false;
 
+    let nextPage!: string|undefined;
+    let prevPage!: string|undefined;
+    let numberOfElements!: number;
+
     return this.pokemonService.getPokemons(page).pipe(
         switchMap((pokemons: PokemonsDto) => {
+
+            nextPage = pokemons.next;
+            prevPage = pokemons.previous;
+            numberOfElements = pokemons.count;
+
             const requests = pokemons.results
                 .map((minifiedPokemon: MinifiedPokemonDto) => 
                     this.pokemonService.getPokemon(minifiedPokemon.name)
                 )
             return forkJoin(requests)
+        }),
+        map((rawPokemons: PokemonDto[]) => {
+            const output: Pokemons = {
+                // to-do
+                numberOfElements,
+                nextPage,
+                prevPage,
+                currentPage: page,
+                data: rawPokemons.map((pokemon): Pokemon => {
+                    return {
+                        name: pokemon.name,
+                        image: pokemon.sprites.front_default
+                    } as Pokemon;
+                })
+            }
+            return output;
         })
     )
   }
