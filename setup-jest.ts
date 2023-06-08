5cf82a96ac14f8e366fac10c5d06aa8357af8fda
Tony Sanjuan <tonysanj13@gmail.com>
2023-06-08 11:00:10 +0200

    feature: added core module as main import for all tests

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,9 @@
 import 'jest-preset-angular/setup-jest';
+import { TestBed } from '@angular/core/testing';
+import { CoreModule } from 'src/app/modules/core/core.module';
+
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    imports: [CoreModule],
+  }).compileComponents();
+});
